cc337f5b64353faf633c38040f5aec0c56122173
MerlinFire <adityakumar5003@gmail.com>
2025-06-27 13:43:42 +0530

    Solved 1st Question

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Hello Github" << endl;
+    cout << "This is my first code to push in my github account" << endl;
+    return 0;
+}
